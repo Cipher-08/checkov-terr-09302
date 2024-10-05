9ca4cc0c8d2a858c29885f2de6e1302d584cf79d
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-10-05 20:32:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,38 @@
-resource "aws_instance" "terrateam-instance" {
-  ami = "ami-0e86e20dae9224db8" 
-  instance_type = "t2.micro"
-  subnet_id = "subnet-02efa144df0a77c13" 
+
+resource "aws_s3_bucket" "vulnerable_bucket" {
+  bucket = "unique-s3-bucket-${random_id.bucket_id.hex}"
+
+  acl    = "public-read" # Vulnerability: Bucket is publicly readable
+
+  versioning {
+    enabled = false # Vulnerability: Versioning is not enabled
+  }
+
+  lifecycle_rule {
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "GLACIER"
+    }
+    expiration {
+      days = 365
+    }
+  }
+
+  logging {
+    target_bucket = "" # Vulnerability: No logging enabled
+  }
 
   tags = {
-    Name        = "TerraTeamInitialInstance"
-    Environment = "Production"
-    Owner       = "Saksham"   
-    Version     = "1.0"        
+    Name        = "Vulnerable S3 Bucket"
+    Environment = "Dev"
   }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 8
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.vulnerable_bucket.bucket
 }
